0dab2de5b610e9785cc63ef87e6ebb03e4b58085
Evan Huang <evan.huang@berkeley.edu>
2022-10-16 11:01:56 -0700

    added more user options for name generator

--- name_generator.py
+++ name_generator.py
@@ -7,16 +7,18 @@ co = cohere.Client(api_key='PoDA0pxkWb7oyLlkLajAd5zqaRWL43U0bxNyW31L')
 
 print('Prediction: {}'.format(response.generations[0].text))"""
 
-def get_summary(compiled_lyrics): 
+def get_summary(compiled_lyrics, model='large', max_tokens=10, 
+temperature=0.8, num_generations=1, k=0, p=1): 
     response = co.generate(
-        model='large',
+        model=model,
         prompt=compiled_lyrics,
-        max_tokens=10,
-        temperature=0.8,
-        num_generations=1,
-        k=0,
-        p=1
+        max_tokens=max_tokens,
+        temperature=temperature,
+        num_generations=num_generations,
+        k=k,
+        p=p
     )
-    print(response.generations[0].text)
+    for i in range(num_generations): 
+        print(response.generations[i].text)
 
-get_summary("I like to eat pie")
+get_summary("I like to eat pie", num_generations=2)
