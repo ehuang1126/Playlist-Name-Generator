74467455360f45cfe3a940673fa1901a8cb71453
Koa Lee <koalee@Koas-MacBook-Pro-14.local>
2022-10-16 00:16:32 -0700

    Updated playlist_fetcher methods

--- playlist_fetcher.py
+++ playlist_fetcher.py
@@ -4,20 +4,21 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_id = "10fc7a6976ad4a759bfacb6d565a6198"
 client_secret = "11c4bb638fc44c7eb17542e64e735eb2"
 
-help_url = "https://open.spotify.com/playlist/5S5QKvTZDKjMjN1M4rauyw?si=b6f9ce9ad5564c24"
+#test_url = "https://open.spotify.com/playlist/5S5QKvTZDKjMjN1M4rauyw?si=b6f9ce9ad5564c24"
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-
-
-
-def getPlaylist(url):
-    playlist = spotify.playlist(url)
-    for track in playlist:
-        #getLyrics(track)
-        print(track)
     
 
-def getLyrics(track):
+def getAllLyrics(url):
+    lyrics = ""
+    playlist = spotify.playlist(url)
+    for track in playlist:
+        lyrics += getSongLyrics(track) + "\n"
+    return lyrics
+
+def getSongLyrics(track):
+    #something to return track lyrics as string
     return
 
-getPlaylist("5S5QKvTZDKjMjN1M4rauyw")
+
+getAllLyrics("5S5QKvTZDKjMjN1M4rauyw")
